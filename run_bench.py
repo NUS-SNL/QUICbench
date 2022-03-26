dc3fcd437ed90a8b7d2d8d34a69a75402b55df88
Sherman <shermz.lim.jh@gmail.com>
2022-03-27 01:38:21 +0800

    add getting sudo privleges and setting of kernel params

--- run_bench.py
+++ run_bench.py
@@ -1,11 +1,15 @@
+import os
+import sys
 import argparse
 import json
+import subprocess
 
 from stacks.chromium import Chromium
 from stacks.msquic import Msquic
 from stacks.mvfst import Mvfst
 from stacks.quiche import Quiche
 from stacks.tcp import Tcp
+from utils.remote_cmd import get_remote_cmd, get_remote_cmd_sudo
 
 
 def get_prog_args():
@@ -16,6 +20,15 @@ def get_prog_args():
         default="./config/general_conf_default.json")
     parser.add_argument("--exp_conf", "-e", help="path to experiment configuration", type=str)
     return parser.parse_args()
+
+
+def check_sudo_privileges(server_hostname, server_pw_path):
+    subprocess.run("sudo echo 'Got sudo privileges for local machine.'", shell=True, check=True)
+    try:
+        subprocess.run(get_remote_cmd_sudo(server_hostname, server_pw_path, "sudo echo 'Got sudo privileges for server machine.'"),
+            shell=True, check=True, timeout=1)
+    except subprocess.TimeoutExpired:
+        sys.exit("exiting... server password is incorrect.")
 
 
 def init_stacks(stacks_conf, server_ip, server_hostname):
@@ -33,14 +46,27 @@ def init_stacks(stacks_conf, server_ip, server_hostname):
     }
 
 
+def set_kernel_params(kernel_params, server_hostname, server_pw_path):
+    print("Setting kernel parameters:")
+    for param, value in kernel_params.items():
+        subprocess.run("sudo sysctl -w {}=\"{}\"".format(param, value), shell=True, check=True)
+        subprocess.run(get_remote_cmd_sudo(server_hostname, server_pw_path, "sudo sysctl -w {}=\\\"{}\\\"".format(param, value)), 
+            shell=True, check=True)
+
+
 def main():
     args = get_prog_args()
     with open(args.stacks_conf) as f:
         stacks_conf = json.load(f)
     with open(args.general_conf) as f:
-        general_conf = json.load(f)        
-
+        general_conf = json.load(f)
+    
+    server_ip, server_hostname, interface = general_conf["server_ip"], general_conf["server_hostname"], general_conf["interface"]
+    server_pw_path = general_conf["server_pw_path"]    
+    check_sudo_privileges(server_hostname, server_pw_path)
+    
     stacks = init_stacks(stacks_conf, general_conf["server_ip"], general_conf["server_hostname"])
+    set_kernel_params(general_conf["kernel_params"], server_hostname, server_pw_path)
 
 
 if __name__ == "__main__":
--- utils/remote_cmd.py
+++ utils/remote_cmd.py
@@ -1,3 +1,6 @@
 
-def get_remote_cmd(remote_hostname, cmd):
-    return ["ssh", remote_hostname, " ".join(cmd)]
+def get_remote_cmd(remote_hostname, cmd_lst):
+    return ["ssh", remote_hostname, " ".join(cmd_lst)]
+
+def get_remote_cmd_sudo(remote_hostname, remote_pw_path, cmd):
+    return "cat {} | ssh {} cat \| sudo --prompt="" -S -- \"{}\"".format(remote_pw_path, remote_hostname, cmd)
