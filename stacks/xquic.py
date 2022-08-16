c9a47af96ee30840295b426747b335cf3385cdbc
shermz-lim <shermz.lim.jh@gmail.com>
2022-08-17 02:48:55 +0800

    add xquic

---
+++ stacks/xquic.py
@@ -0,0 +1,53 @@
+import subprocess
+from utils.remote_cmd import get_remote_cmd
+from stacks.stack import Stack
+
+class Xquic(Stack):
+    NAME = "xquic"
+    CUBIC = "cubic"
+    BBR = "bbr"
+    BBRV2 = "bbrv2"
+    RENO = "reno"
+
+    CC_NAME_MAP = {
+        "cubic": "c",
+        "bbr": "b",
+        "bbrv2": "B",
+        "reno": "r"
+    }
+
+    def __init__(self, server_ip, server_hostname, server_path, client_path):
+        self.server_ip = server_ip
+        self.server_hostname = server_hostname
+        self.server_path = server_path
+        self.client_path = client_path
+
+    def run_remote_server(self, port_no, cc_algo, duration_s):
+        cmd = self.run_server_cmd(port_no, cc_algo, duration_s)
+        cmd = get_remote_cmd(self.server_hostname, cmd)
+        return subprocess.Popen(cmd)
+
+    def run_client(self, port_no, cc_algo, duration_s):
+        cmd = self.run_client_cmd(port_no, duration_s)
+        return subprocess.Popen(" ".join(cmd), shell=True)
+
+    def run_server_cmd(self, port_no, cc_algo, duration_s):
+        return map(str, [
+            "cd {} &&".format(self.server_path),
+            "timeout", duration_s,
+            "./test_server -s 1048576000 -c {}".format(Xquic.CC_NAME_MAP[cc_algo]),
+            "-p {}".format(port_no),
+            "> /dev/null 2>&1"
+        ])
+
+    def run_client_cmd(self, port_no, duration_s):
+        return map(str, [
+            "cd {} &&".format(self.client_path),
+            "timeout", duration_s,
+            "./test_client -d 0 -a {} -p {}".format(self.server_ip, port_no),
+            "> /dev/null 2>&1"
+        ])
+
+    @staticmethod
+    def get_cc_algos():
+        return [Xquic.CUBIC, Xquic.BBR, Xquic.BBRV2, Xquic.RENO]
