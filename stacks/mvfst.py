08b2ca11e4ab9a920107ad0c7d8d867076b41fc4
shermz-lim <shermz.lim.jh@gmail.com>
2022-09-02 03:59:59 +0800

    add mvfst stack logging

--- stacks/mvfst.py
+++ stacks/mvfst.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from utils.remote_cmd import get_remote_cmd
 from stacks.stack import Stack
@@ -19,6 +20,11 @@ class Mvfst(Stack):
         cmd = get_remote_cmd(self.server_hostname, cmd)
         return subprocess.Popen(cmd)
 
+    def run_remote_server_wlogs(self, port_no, cc_algo, duration_s, log_path):
+        cmd = self.run_server_cmd_wlogs(port_no, cc_algo, duration_s, log_path)
+        cmd = get_remote_cmd(self.server_hostname, cmd)
+        return subprocess.Popen(cmd)
+
     def run_client(self, port_no, cc_algo, duration_s):
         cmd = self.run_client_cmd(port_no, duration_s)
         return subprocess.Popen(cmd)
@@ -29,6 +35,16 @@ class Mvfst(Stack):
             self.server_path, "-mode=server", "-host=0.0.0.0", "-pacing=true",
             "-port={}".format(port_no), "-congestion={}".format(cc_algo)
         ])
+
+    def run_server_cmd_wlogs(self, port_no, cc_algo, duration_s, log_path):
+        log_dir = os.path.dirname(log_path)
+        return map(str, [
+            "timeout", duration_s,
+            self.server_path, "-mode=server", "-host=0.0.0.0", "-pacing=true",
+            "-port={}".format(port_no), "-congestion={}".format(cc_algo),
+            "-server_qlogger_path={};".format(log_dir),
+            "mv {} {}".format(os.path.join(log_dir, "*.qlog"), log_path)
+        ])        
 
     def run_client_cmd(self, port_no, duration_s):
         return map(str, [
