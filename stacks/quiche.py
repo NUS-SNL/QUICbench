4925e43fae19f3d107f51cb205d9e8e7b7a8aa5d
shermz-lim <shermz.lim.jh@gmail.com>
2022-09-02 03:53:21 +0800

    add quiche server logging

--- stacks/quiche.py
+++ stacks/quiche.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from utils.remote_cmd import get_remote_cmd
 from stacks.stack import Stack
@@ -27,6 +28,11 @@ class Quiche(Stack):
         cmd = get_remote_cmd(self.server_hostname, cmd)
         return subprocess.Popen(cmd)
 
+    def run_remote_server_wlogs(self, port_no, cc_algo, duration_s, log_path):
+        cmd = self.run_server_cmd_wlogs(port_no, cc_algo, duration_s, log_path)
+        cmd = get_remote_cmd(self.server_hostname, cmd)
+        return subprocess.Popen(cmd)
+
     def run_client(self, port_no, cc_algo, duration_s):
         cmd = self.run_client_cmd(port_no, duration_s)
         return subprocess.Popen(" ".join(cmd), shell=True)
@@ -39,6 +45,18 @@ class Quiche(Stack):
             "--listen 0.0.0.0:{}".format(port_no), "--root {}".format(self.server_static_file_dir),
             "--index {}".format(self.server_static_filename), "--cc-algorithm {}".format(cc_algo)
         ])
+
+    def run_server_cmd_wlogs(self, port_no, cc_algo, duration_s, log_path):
+        log_dir = os.path.dirname(log_path)
+        return map(str, [
+            "export QLOGDIR={};".format(log_dir),
+            "timeout", duration_s,
+            "{} run --manifest-path={} --bin quiche-server --".format(self.server_cargo_path, self.server_path),
+            "--cert {}".format(self.server_cert_path), "--key {}".format(self.server_key_path),
+            "--listen 0.0.0.0:{}".format(port_no), "--root {}".format(self.server_static_file_dir),
+            "--index {}".format(self.server_static_filename), "--cc-algorithm {}".format(cc_algo), ";",
+            "mv {} {}".format(os.path.join(log_dir, "*.sqlog"), log_path)
+        ])        
 
     def run_client_cmd(self, port_no, duration_s):
         return map(str, [
