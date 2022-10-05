6375151b745a9f6c997fe79242cba9b3d01030bd
shermz-lim <shermz.lim.jh@gmail.com>
2022-10-06 00:54:16 +0800

    add reno to quic-go

--- stacks/quicgo.py
+++ stacks/quicgo.py
@@ -5,38 +5,34 @@ from stacks.stack import Stack
 class QuicGo(Stack):
     NAME = "quicgo"
     CUBIC = "cubic"
+    RENO = "reno"
 
     def __init__(self, server_ip, server_hostname, server_pw_path,
-                 go_path, server_path, server_static_file_dir,
+                 go_path, cubic_server_path, reno_server_path, server_static_file_dir,
                  server_static_filename, client_path):
         self.server_ip = server_ip
         self.server_hostname = server_hostname
         self.go_path = go_path
-        self.server_path = server_path
+        self.server_paths = {
+            QuicGo.CUBIC: cubic_server_path,
+            QuicGo.RENO: reno_server_path,
+        }
         self.server_static_file_dir = server_static_file_dir
         self.server_static_filename = server_static_filename
         self.client_path = client_path
 
     def run_remote_server(self, port_no, cc_algo, duration_s):
-        # there's only CUBIC for quic go
-        if cc_algo != QuicGo.CUBIC:
-            raise ValueError("{} is not a valid cc_algo for quicgo".format(cc_algo))
-
         cmd = self.run_server_cmd(port_no, cc_algo, duration_s)
         cmd = get_remote_cmd(self.server_hostname, cmd)
         return subprocess.Popen(cmd)
 
     def run_client(self, port_no, cc_algo, duration_s):
-        # there's only CUBIC for quic go
-        if cc_algo != QuicGo.CUBIC:
-            raise ValueError("{} is not a valid cc_algo for quicgo".format(cc_algo))
-
         cmd = self.run_client_cmd(port_no, duration_s)
         return subprocess.Popen(" ".join(cmd), shell=True)
 
     def run_server_cmd(self, port_no, cc_algo, duration_s):
         return map(str, [
-            "cd {} &&".format(self.server_path),
+            "cd {} &&".format(self.server_paths[cc_algo]),
             "timeout", duration_s,
             "{} run main.go -www {}".format(self.go_path, self.server_static_file_dir),
             "-bind 0.0.0.0:{}".format(port_no)
@@ -53,4 +49,4 @@ class QuicGo(Stack):
 
     @staticmethod
     def get_cc_algos():
-        return [QuicGo.CUBIC]
+        return [QuicGo.CUBIC, QuicGo.RENO]
