2e7708e28aa15a7579cc0da9f5b6fd8fa2e17116
shermz-lim <shermz.lim.jh@gmail.com>
2024-09-26 22:09:43 +0800

    adds aioquic

---
+++ stacks/aioquic.py
@@ -0,0 +1,54 @@
+import subprocess
+from utils.remote_cmd import get_remote_cmd
+from stacks.stack import Stack
+
+class Aioquic(Stack):
+    NAME = "aioquic"
+    CUBIC = "cubic"
+    RENO = "reno"
+
+    def __init__(self, server_ip, server_hostname, server_pw_path,
+                 server_path, server_cert_path, server_key_path,
+                 server_static_file_dir, server_static_filename,
+                 client_path, ca_path):
+        self.server_ip = server_ip
+        self.server_hostname = server_hostname
+        self.server_path = server_path
+        self.server_cert_path = server_cert_path
+        self.server_key_path = server_key_path
+        self.server_static_file_dir = server_static_file_dir
+        self.server_static_filename = server_static_filename
+        self.client_path = client_path
+        self.ca_path = ca_path
+
+    def run_remote_server(self, port_no, cc_algo, duration_s):
+        cmd = self.run_server_cmd(port_no, cc_algo, duration_s)
+        cmd = get_remote_cmd(self.server_hostname, cmd)
+        return subprocess.Popen(cmd)
+
+    def run_client(self, port_no, cc_algo, duration_s):
+        cmd = self.run_client_cmd(port_no, duration_s)
+        return subprocess.Popen(" ".join(cmd), shell=True)
+
+    def run_server_cmd(self, port_no, cc_algo, duration_s):
+        # configs taken from https://github.com/aiortc/aioquic/blob/1.2.0/examples/http3_server.py
+        return map(str, [
+            "timeout", duration_s,
+            "python3 {} --certificate {}".format(self.server_path, self.server_cert_path),
+            "--private-key {}".format(self.server_key_path),
+            "--congestion-control-algorithm {} ".format(cc_algo),
+            "--host 0.0.0.0 --port {}".format(port_no)
+        ])
+
+    def run_client_cmd(self, port_no, duration_s):
+        return map(str, [
+            "timeout", duration_s,
+            "python3 {}  --insecure".format(self.client_path),
+            "--ca-certs {}".format(self.ca_path),
+             " https://{}:{}".format(self.server_ip, port_no),
+            "> /dev/null 2>&1"
+        ])
+
+    @staticmethod
+    def get_cc_algos():
+        return [Aioquic.CUBIC, Aioquic.RENO]
