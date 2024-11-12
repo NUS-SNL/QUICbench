07cbf7f0f8418e533bcd9b2c36c3e9d85d5555f4
Benjamin Long <benjaminlong@u.nus.edu>
2024-11-12 12:41:30 +0800

    fix: fixes parse_pcap looking for veth

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 # FILE NAMING CONVENTIONS
 INTERFACE_PCAP_FILENAME = "packets.pcap"
-VETH_PCAP_FILENAME = "packets-br0.pcap"
+VETH_PCAP_FILENAME = "packets.pcap"
+# VETH_PCAP_FILENAME = "packets-br0.pcap"
 THROUGHPUT_TRACE_SUFFIX = ".tp-trace"
 DELAY_TRACE_SUFFIX = ".delays"
 STACK_LOG_SUFFIX = ".log"
