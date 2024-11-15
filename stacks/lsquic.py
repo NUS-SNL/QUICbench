a0b13cfba03e6fd0493dda2facb6888d8ec60fbc
Benjamin Long <benjaminlong@u.nus.edu>
2024-11-16 01:21:13 +0800

    fix: lsquic

--- stacks/lsquic.py
+++ stacks/lsquic.py
@@ -8,6 +8,13 @@ class Lsquic(Stack):
     BBR = "bbr"
 
     # https://github.com/litespeedtech/lsquic/blob/master/include/lsquic.h
+    """
+    -A CC_ALGO  Congestion control algorithm.  The following algorithms are
+                supported.
+                1: Cubic
+                2: BBRv1
+                3: Adaptive congestion control (this is the default).
+    """
     CC_NAME_NO_MAP = {
         "cubic": "1",
         "bbr": "2",
@@ -29,11 +36,13 @@ class Lsquic(Stack):
     def run_remote_server(self, port_no, cc_algo, duration_s):
         cmd = self.run_server_cmd(port_no, cc_algo, duration_s)
         cmd = get_remote_cmd(self.server_hostname, cmd)
+        print(" ".join(cmd))
         return subprocess.Popen(cmd)
 
     def run_client(self, port_no, cc_algo, duration_s):
         cmd = self.run_client_cmd(port_no, duration_s)
         newcmd = " ".join(cmd)
+        print(newcmd)
         return subprocess.Popen(newcmd, shell=True)
 
     def run_server_cmd(self, port_no, cc_algo, duration_s):
@@ -41,7 +50,7 @@ class Lsquic(Stack):
             "timeout", duration_s,
             "{} -c www.lsquic.com,{},{}".format(self.server_path, self.server_cert_path, self.server_key_path),
             "-r {} -p {}".format(self.server_static_file_dir, self.server_static_filename),
-            "-s 0.0.0.0:{} -o cc_algo={}".format(port_no, Lsquic.CC_NAME_NO_MAP[cc_algo])
+            "-s 0.0.0.0:{} -A {}".format(port_no, Lsquic.CC_NAME_NO_MAP[cc_algo])
         ])
 
     def run_client_cmd(self, port_no, duration_s):
